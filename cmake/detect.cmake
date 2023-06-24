2d2b3c01b9000cca2340a8de424d4d9a41957fcf
Eugene Gershnik <gershnik@hotmail.com>
2023-06-24 00:13:13 -0700

    Correct check for mkostemps

--- cmake/detect.cmake
+++ cmake/detect.cmake
@@ -3,11 +3,11 @@ include(CheckCXXSymbolExists)
 include(CheckCXXSourceCompiles)
 include(CheckCXXSourceRuns)
 
-check_cxx_symbol_exists(mkostemps unistd.h PTL_HAVE_MKOSTEMPS)
 check_cxx_symbol_exists(execvpe unistd.h PTL_HAVE_EXECVPE)
 check_cxx_symbol_exists(posix_spawn_file_actions_addinherit_np spawn.h PTL_HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDINHERIT_NP)
 check_cxx_symbol_exists(posix_spawn_file_actions_addchdir_np spawn.h PTL_HAVE_POSIX_SPAWN_FILE_ACTIONS_ADDCHDIR_NP)
 check_cxx_symbol_exists(sigabbrev_np string.h PTL_HAVE_SIGABBREV_NP)
+check_cxx_symbol_exists(sys_signame signal.h PTL_HAVE_SYS_SIGNAME)
 check_cxx_source_runs("
     #include <signal.h>
     #include <stdio.h>
@@ -19,4 +19,12 @@ check_cxx_source_runs("
         printf(\"%s\", x);
     }" 
 PTL_HAVE_SYS_SIGABBREV)
-check_cxx_symbol_exists(sys_signame signal.h PTL_HAVE_SYS_SIGNAME)
+check_cxx_source_runs("
+    #include <unistd.h>
+    #include <stdlib.h>
+    
+    int main() { 
+        int (*p)(char *, int, int) = mkostemps;
+    }" 
+PTL_HAVE_MKOSTEMPS)
+

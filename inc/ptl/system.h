94e9bd3b390ee07c0216c5c5713723bdba6ab452
Eugene Gershnik <gershnik@hotmail.com>
2023-07-02 00:48:21 -0700

    Adding `gethostname`

--- inc/ptl/system.h
+++ inc/ptl/system.h
@@ -4,7 +4,9 @@
 #include <ptl/core.h>
 
 #include <optional>
+#include <span>
 
+#ifndef _WIN32
 
 namespace ptl::inline v0 {
 
@@ -20,7 +22,17 @@ namespace ptl::inline v0 {
         return ret;
     }
 
+    inline void getHostName(std::span<char> buf, PTL_ERROR_REF_ARG(err)) 
+    requires(PTL_ERROR_REQ(err)) {
+        if (::gethostname(buf.data(), buf.size()) != 0)
+            handleError(PTL_ERROR_REF(err), res, "gethostname(...,{}) failed", buf.size());
+        else
+            clearError(PTL_ERROR_REF(err));
+    }
+
 }
+
+#endif
 
 
 #endif

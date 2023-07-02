75479cfacdd1b2cae159c9305bc8b5c6ae294cf3
Eugene Gershnik <gershnik@hotmail.com>
2023-07-02 00:50:19 -0700

    Fixing typo

--- inc/ptl/system.h
+++ inc/ptl/system.h
@@ -25,7 +25,7 @@ namespace ptl::inline v0 {
     inline void getHostName(std::span<char> buf, PTL_ERROR_REF_ARG(err)) 
     requires(PTL_ERROR_REQ(err)) {
         if (::gethostname(buf.data(), buf.size()) != 0)
-            handleError(PTL_ERROR_REF(err), res, "gethostname(...,{}) failed", buf.size());
+            handleError(PTL_ERROR_REF(err), errno, "gethostname(...,{}) failed", buf.size());
         else
             clearError(PTL_ERROR_REF(err));
     }

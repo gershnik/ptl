66334305c34f43b9d75d7a775dc877ae03bdf192
Eugene Gershnik <gershnik@hotmail.com>
2023-07-10 17:40:53 -0700

    Tweaks to tests for Android

--- test/test_users.cpp
+++ test/test_users.cpp
@@ -21,7 +21,7 @@ struct UserInfo {
 static auto getMyself() -> const UserInfo & {
     static UserInfo theInfo = []() {
         return UserInfo {
-            .user = shell({ "whoami" }),
+            .user = shell({ "id", "-un" }),
             .uid = uid_t(atoi(shell({ "id", "-u" }).c_str())),
             .gid = gid_t(atoi(shell({ "id", "-g" }).c_str()))
         };

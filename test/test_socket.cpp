28194d6e03f5431bc1e2a9b160a6df3961647178
Eugene Gershnik <gershnik@hotmail.com>
2023-07-25 01:11:42 -0700

    Adding socket bind/send/recv calls

--- test/test_socket.cpp
+++ test/test_socket.cpp
@@ -7,6 +7,8 @@
 
 #include "common.h"
 
+#include <thread>
+
 using namespace ptl;
 
 TEST_CASE( "socket creation" , "[socket]") {
@@ -68,4 +70,34 @@ TEST_CASE( "socket options" , "[socket]") {
         CHECK(lingerOut.l_onoff != 0);
         CHECK(linger.l_linger == lingerOut.l_linger);
     }
+}
+
+TEST_CASE( "read-write" , "[socket]") {
+
+    auto recvSock = createSocket(PF_INET, SOCK_DGRAM, 0);
+    sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = 0;
+    addr.sin_addr.s_addr = 0;
+    ptl::socklen_t addrLen = sizeof(addr);
+
+    bindSocket(recvSock, reinterpret_cast<sockaddr *>(&addr), addrLen);
+    getSocketName(recvSock, reinterpret_cast<sockaddr *>(&addr), &addrLen);
+    REQUIRE(addrLen == sizeof(addr));
+
+    auto sendSock = createSocket(PF_INET, SOCK_DGRAM, 0);
+
+    ptl::socklen_t sent;
+    std::error_code ec;
+    std::thread sender([&](){
+        sent = sendSocket(sendSock, "hello", 5, 0, reinterpret_cast<sockaddr *>(&addr), addrLen, ec);
+    });
+    
+    char buf[6];
+    auto received = receiveSocket(recvSock, buf, sizeof(buf), 0);
+    sender.join();
+    CHECK(!ec);
+    CHECK(sent == 5);
+    CHECK(received == 5);
+    CHECK(strcmp(buf, "hello") == 0);
 }
